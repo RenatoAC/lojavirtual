c24b9a12752464fc2677b1cfb1c6a2f2b92ef06a
RenatoAC <renato.agricio@gmail.com>
2022-04-03 17:29:36 -0300

    Testando a primeira classe de dominio - Categoria

--- src/main/java/com/renatoac/lojavirtual/resources/CategoriaResource.java
+++ src/main/java/com/renatoac/lojavirtual/resources/CategoriaResource.java
@@ -1,16 +1,31 @@
 package com.renatoac.lojavirtual.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.renatoac.lojavirtual.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Camiseta");
+		Categoria cat2 = new Categoria(1, "Bermuda");
+		Categoria cat3 = new Categoria(3, "Vestido");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		lista.add(cat3);
+		
+		return lista;
 	}
 
 }
