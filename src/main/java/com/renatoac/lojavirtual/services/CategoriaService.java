447fa608939abee00c1378b15f28278e4fddb093
RenatoAC <renato.agricio@gmail.com>
2022-04-03 21:50:41 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/renatoac/lojavirtual/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.renatoac.lojavirtual.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.renatoac.lojavirtual.domain.Categoria;
+import com.renatoac.lojavirtual.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id); 
+		return obj.orElse(null); 
+	}
+
+}
