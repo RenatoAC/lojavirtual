52ad2cff68752fe82d34e18d353573e936714770
RenatoAC <renato.agricio@gmail.com>
2022-04-04 15:34:32 -0300

    Criando operacao de instanciacao

--- src/main/java/com/renatoac/lojavirtual/LojavirtualApplication.java
+++ src/main/java/com/renatoac/lojavirtual/LojavirtualApplication.java
@@ -1,13 +1,33 @@
 package com.renatoac.lojavirtual;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class LojavirtualApplication {
+import com.renatoac.lojavirtual.domain.Categoria;
+import com.renatoac.lojavirtual.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class LojavirtualApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LojavirtualApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Camisetas");
+		Categoria cat2 = new Categoria(null, "Bermudas");
+		Categoria cat3 = new Categoria(null, "Vestidos");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 	}
 
 }
